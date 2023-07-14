ae3197365159df67ebd1eabbbdcad16fedfb336d
Daniel Hurtado Quero <131759030+OconnelDan@users.noreply.github.com>
2023-07-14 15:58:29 +0000

    añadido estilos

--- src/js/component/Tareas.jsx
+++ src/js/component/Tareas.jsx
@@ -27,11 +27,11 @@ export const ListaTareas = () => {
 
 
     return (
-    <div className="container">
+    <div className="container" id="Tareas">
             <div className="header">
                 <h1 className="text-muted" >Tareas</h1>
             </div>
-            <div className="input-group input-group-lg">
+            <div className="input-group input-group-lg" >
                   <input type="text" onKeyDown={handleKeyDown} onChange={e=>setInputValue(e.target.value)} value={inputValue} className="form-control" placeholder="Añade una tarea"  aria-describedby="inputGroup-sizing-lg"/>
             </div>
             <ul className="list-group listaTarea">
